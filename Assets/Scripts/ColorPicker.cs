58253a2b8fbb5d55a35df867f7c4c8c8f401945c
Mourette Ferdinand <73692928+Ferdi-git@users.noreply.github.com>
2026-01-21 13:01:27 +0100

    r

---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPicker : MonoBehaviour
+{
+    [SerializeField] Image colorDisplay;
+
+    [Serializable]
+    public enum ColorOption
+    {
+        Red,
+        Green,
+        Blue,
+    }
+    
+    public void ChangeColorR(float value)
+    {
+        colorDisplay.color = new Color(value, colorDisplay.color.g, colorDisplay.color.b);
+        
+    }
+
+    public void ChangeColorG(float value)
+    {
+        colorDisplay.color = new Color(colorDisplay.color.r, value, colorDisplay.color.b);
+    }
+    public void ChangeColorB(float value)
+    {
+        colorDisplay.color = new Color(colorDisplay.color.r, colorDisplay.color.g, value);
+    }
+
+}
